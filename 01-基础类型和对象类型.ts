ef4b9977a1576401bb0003ed1fece1a4b3e9abf6
何智 <hezhi@yunzujia.com>
2021-03-29 23:51:08 +0800

    feat: 基础类型补充

--- 01-基础类型和对象类型.ts
+++ 01-基础类型和对象类型.ts
@@ -1,8 +1,8 @@
-// 基础类型 number, undefined, symbol, boolean, void
+// 基础类型 boolean, number, string, undefined, symbol, void, null
 const count: number = 123;
 const sName: string = 'hello';
 
-// 对象类型
+// 对象类型 {}, Class, function, []
 const teacher: {
   name: string;
   age: number;
@@ -15,7 +15,24 @@ const numbers: number[] = [1, 2, 3];
 class Person {}
 const deil: Person = new Person();
 
-// 函数 () => number 表示该函数是一个返回数字的函数
-const getTotal: () => number = () => {
-  return 123;
+// 方式1. str:string 表示参数是string类型, :number 表示返回值是number
+const getNum1 = (str: string): number => {
+  return parseInt(str, 10);
 };
+// 方式2 (str:string) => number 也同样可以表示该函数参数是string，返回值是number。最后再写真正的函数体 =(str) => {..}
+const getNum2: (str: string) => number = (str) => {
+  return parseInt(str, 10);
+};
+// 方式1 还可以利用TS类型推断的特性简写一下，鼠标移到getNum3上，会发现TS根据parseInt的提示自动推断出了该函数的返回值是number类型
+const getNum3 = (str: string) => {
+  return parseInt(str, 10);
+};
+// 总结：有些函数的入参类型需要手动写，但是返回值往往可以TS的类型推断自动推断出来，可以不写
+
+const data = new Date(); // => 等同于  const data: Date = new Date(); 也是因为TS类型推断自动推断出这是一个Date类型
+
+/**
+ * | 可以让变量在多个类型之间变化
+ */
+let temp: number | string = 123;
+temp = '456';

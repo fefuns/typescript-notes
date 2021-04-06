fceeb9f9fe1ff4349faf41b9322ac902c5c7b49a
fefuns <1321120469@qq.com>
2021-04-06 20:57:08 +0800

    fix: 16-描述文件中的全局类型

--- 16-描述文件中的全局类型.ts
+++ 16-描述文件中的全局类型.ts
@@ -1,5 +1,5 @@
 /**
- * 比如引入一个jquery的时候，页面上会写
+ * 比如通过<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js"></script>引入一个jquery的时候，页面上会写
  * $(function() {
  *    console.log(1);
  * })
@@ -13,18 +13,18 @@ interface JqueryInstance {
   html: (html: string) => JqueryInstance;
 }
 // 定义全局函数与函数重载
-// declare function $(readyFunc: () => void): void;
-// declare function $(selector: string): JqueryInstance;// 定义了这个之后，就可以使用 $('body').html('<p>11</p>')
+declare function $(readyFunc: () => void): void;
+declare function $(selector: string): JqueryInstance; // 定义了这个之后，就可以使用 $('body').html('<p>11</p>')
 
 // 如何对对象进行类型定义，以及对类进行类型定义，以及命名空间的嵌套
-// declare namespace $ {
-//   namespace fn {
-//     class init {} // 此时可以写 $.fn.init()方法了
-//   }
-// }
-// 使用 interface 的语法，实现函数重载
-interface JQuery {
-  (readyFunc: () => void): void;
-  (selector: string): JqueryInstance;
+declare namespace $ {
+  namespace fn {
+    class init {} // 此时可以写 $.fn.init()方法了
+  }
 }
-declare var $: JQuery;
+// 使用 interface 的语法，实现函数重载，这种方法不能定义对象
+// interface JQuery {
+//   (readyFunc: () => void): void;
+//   (selector: string): JqueryInstance;
+// }
+// declare var $: JQuery;
